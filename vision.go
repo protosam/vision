7eb01dfdfd16d1752245428002166b2d07c617b8
Sam <samigknighted@gmail.com>
2015-12-26 07:20:37 -0600

    Global assignments added

--- vision.go
+++ vision.go
@@ -11,6 +11,7 @@ type New struct {
 	output string
 	blocks map[string]string
 	Assignments map[string]string
+	GAssignments map[string]string
 }
 
 func (tpl *New) TemplateFile(tpl_file string) {
@@ -26,6 +27,7 @@ func (tpl *New) TemplateFile(tpl_file string) {
 
 	tpl.blocks = make(map[string]string)
 	tpl.Assignments = make(map[string]string)
+	tpl.GAssignments = make(map[string]string)
 
 	tpl.output = tpl.parseblocks(tpl.output, "")
 
@@ -37,10 +39,19 @@ func (tpl *New) Assign(name string, value string) {
 	value = strings.Replace(value, "}", "_SANITIZED}", -1)
 	tpl.Assignments[name] = value
 }
+func (tpl *New) GAssign(name string, value string) {
+	// sanitize the value
+	value = strings.Replace(value, "}", "_SANITIZED}", -1)
+	tpl.GAssignments[name] = value
+}
 
 
 func (tpl *New) Parse(block_name string) {
 	tpl.output = strings.Replace(tpl.output, "<!-- BLOCK: " + block_name + " -->", tpl.blocks[block_name] + "<!-- BLOCK: " + block_name + " -->", 1)
+	for name, value := range tpl.GAssignments {
+		tpl.output = strings.Replace(tpl.output, "{"+name+"}", value, -1)
+	}
+	
 	for name, value := range tpl.Assignments {
 		tpl.output = strings.Replace(tpl.output, "{"+name+"}", value, -1)
 	}
